04104717ccbd00087effff458e843da89359f493
Freya De Vos <freya.de.vos@student.ehb.be>
2025-05-07 21:47:14 +0200

    fixing css issues op isntruction page van flex naar grid

--- src/components/InstructionPage.jsx
+++ src/components/InstructionPage.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
-import PageIndicators from './PageIndicators';  // Zorg ervoor dat je deze component hebt
-import ExitButton from './ExitButton';  // Zorg ervoor dat je deze component hebt
-import styles from './InstructionPage.module.css';
+import PageIndicators from './PageIndicators';
+import ExitButton from './ExitButton';
 import Animation from './Animation';
 import Start from './Start';
+import styles from './LayoutGrid.module.css';
+'./Animation.module.css';
 
 const InstructionPage = ({ onNextPage }) => {
   return (
-    <div className={styles.instructionPageContainer}>
-      <PageIndicators totalPages={6} currentPage={1} onPageChange={() => {}} />
-      <ExitButton onClick={() => alert('Exit!')} />
-     <h1>instructions</h1>
-     <NextArrow/>
-     <div className={styles.animationInstructionContainer}>
-        <div className={styles.animationBlock}>
-        <Animation />
-        <p>zet de koptelefoon op</p>
-        </div>
-        <div className={styles.animationBlock}>
-        <Animation />
-        <p>zet de koptelefoon op</p>
-        </div>
+    <div className={styles.gridContainer}>
+      {/* Header */}
+      <div className={styles.header}>
+        <PageIndicators totalPages={6} currentPage={1} onPageChange={() => {}} />
+        <ExitButton onClick={() => alert('Exit!')} />
       </div>
-      <Start/>
+
+      {/* Main content */}
+      <div className={styles.mainContent}>
+        <h1>Instructions</h1>
+        <div className={styles.animationInstructionContainer}>
+          <div className={styles.animationBlock}>
+            <Animation />
+            <p>Zet de koptelefoon op</p>
+          </div>
+          <div className={styles.animationBlock}>
+            <Animation />
+            <p>Doe de Polar meter om</p>
+          </div>
+         
+        </div>
+        <Start />
+      </div>
+
+      {/* Footer */}
+      <div className={styles.footer}>
+      
+      </div>
     </div>
   );
 };
